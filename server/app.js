bc0b2a868d2283bca4e602f7ed12883aea1fda16
Anthony Eneh <aeneh90@gmail.com>
2018-07-27 14:34:38 -0700

    Bug: Organize folder structure

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const model = require('./models/model.js');
 const app = express();
 
 app.get('/entries', (req, res) => {
-    res.writeHead(200, 'OK', {"content-type":"text/json"})
-    res.write(model.entries);    //res.writeHead(200, 'OK');
-    res.end();
+  res.writeHead(200, 'OK', { 'content-type': 'text/json' });
+  res.write(model.entries); // res.writeHead(200, 'OK');
+  res.end();
 
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
