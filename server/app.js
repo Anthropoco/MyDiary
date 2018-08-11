6ca1b08cf7731992428bd0b9549486a5855c29fd
Anthony Eneh <aeneh90@gmail.com>
2018-08-12 00:51:02 +0100

    (ft) Create router files

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import entryRouter from './routers/entries.js';
 
 const app = express();
 
-app.use('/users', userRouter );
+app.use('/users', userRouter);
 app.use('/entries', entryRouter);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
---
+++ server/routers/entries.js
@@ -0,0 +1,7 @@
+import entriesController from './controllers/entries';
+
+const entriesRouter = (req, res, next) => {
+    app.all('/entries', entriesController);
+}
+
+export entriesRouter;
