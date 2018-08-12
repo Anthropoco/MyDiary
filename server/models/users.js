91afaf989b6ddaa024ca4592931dee9e2bdc708a
Anthony Eneh <aeneh90@gmail.com>
2018-08-12 03:13:01 +0100

    (ft) Create skeleton of users model

---
+++ server/models/users.js
@@ -0,0 +1,13 @@
+const usersModel = {
+    getAllUsers(){},
+
+    getUser(){},
+    
+    createUser(data){},
+    
+    modifyUser(userId, data){},
+    
+    deleteUser(userId){}
+}
+
+export usersModel;
