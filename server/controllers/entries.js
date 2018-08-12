9583f7ba14e2e052ebf7128ea877cd9a2093f29f
Anthony Eneh <aeneh90@gmail.com>
2018-08-12 01:54:46 +0100

    (ft) Create user and entry controllers

---
+++ server/controllers/entries.js
@@ -0,0 +1,59 @@
+import entriesModel from './models/entries';
+
+const entriesController = (req, res) => {
+    let users = usersModel.getAllUsers();
+
+    //if user isn't a member, redirect to home page
+    if (!(users.indexof(user))) { res.send(400, "User doesn't exist")};
+
+
+
+    //get list of entries
+    app.get('/entries', (req, res) => {
+        const entries = entriesModel.getAllEntries();
+        res.json(entries);
+    });
+
+
+    //get a particular entry
+    app.get('/entries/:id', (req, res) => {
+        const entry = entriesModel.getUser(req.params.id);
+        res.json(entry);
+
+    });
+
+
+    //create a new entry
+    app.post('/entries', (req, res) => {
+        const newEntry = entriesModel.createEntry(req.body);
+        if (newEntry) {
+            res.send(200, "entry was created successfully");
+        } else {
+            res.send(400, "sorry we couldn't create the entry. Try again");
+        }
+    })
+
+
+    //modify an existing entry
+    app.put('/entries/:id', (req, res) => {
+        const changedEntry = entriesModel.modifyEntry(req.params.id);
+        if (changedEntry) {
+            res.send(200, "entry was modified successfully");
+        } else {
+            res.send(400, "sorry we couldn't modify the entry. Try again");
+        }
+    })
+
+
+    //delete an entry
+    app.delete('/entries/:id', (req, res) => {
+        const deletedEntry = entriesModel.deleteEntry(req.params.id);
+        if (deletedEntry) {
+            res.send(200, "entry was deleted successfully");
+        } else {
+            res.send(400, "sorry we couldn't deleted the entry. Try again");
+        }
+    })
+}
+
+export entriesController;
---
+++ server/controllers/users.js
@@ -0,0 +1,46 @@
+import usersModel from './models/users';
+
+const usersController = (req, res) => {
+    const usersController = (req, res) => {
+        let users = usersModel.getAllUsers();
+
+        //if user isn't a member, redirect to home page
+        if (!(users.indexof(user))) { res.send(400, "User doesn't exist") };
+
+
+
+        //user login
+        app.get('/users/:id', (req, res) => {
+            const user = usersModel.getUser(req.params.id, req.body.password);  //parameters are username and password
+            if (user) {
+                res.send(200, "successfully logged in")
+            } else {
+                res.send(400, "login was not successful. Try again")
+            }
+
+        });
+
+
+        //create a new entry
+        app.post('/users', (req, res) => {
+            const newUser = usersModel.createUser(req.body);
+            if (newUser) {
+                res.send(200, "entry was created successfully");
+            } else {
+                res.send(400, "sorry we couldn't create the entry. Try again");
+            }
+        })
+
+
+        //modify an existing entry
+        app.put('/entries/:id', (req, res) => {
+            const changedUser = usersModel.modifyUser(req.params.id);
+            if (changedUser) {
+                res.send(200, "user info was modified successfully");
+            } else {
+                res.send(400, "sorry we couldn't modify the user info. Try again");
+            }
+        })
+    }
+
+export usersController;
